5ba30840481deec58230a4bfd4d073d434db50dd
Crispelo <penagoscristian61@gmail.com>
2023-03-20 17:03:16 -0500

    add routes of administrador app and views organizing the panelAdministration (no cambies el id del canvas) , change of name of the file css

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from administrador.views import activosTotales, loginPanel
+from administrador.views import activosTotales
 
 urlpatterns = [
     path('login/dashboard',activosTotales, name='dashboard'),
