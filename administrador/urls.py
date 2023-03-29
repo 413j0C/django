b2082239c55504ef89dfb0afc9b35bb033f9e97a
Crispelo <penagoscristian61@gmail.com>
2023-03-28 23:59:51 -0500

    creation of view for the html of registroActivos

--- administrador/urls.py
+++ administrador/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from administrador.views import activosTotales
+from administrador.views import activosTotales,formatoPdf
 
 urlpatterns = [
     path('login/dashboard',activosTotales, name='dashboard'),
+    path('login/registropdf',formatoPdf,name="registropdf")
 ]
