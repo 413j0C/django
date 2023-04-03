449b3e97e034e14ee4f90a9741b12078cb1c3e01
Crispelo <penagoscristian61@gmail.com>
2023-04-03 16:04:08 -0500

    adding the method pageSize of the field button.js

--- static/js/buttons.js
+++ static/js/buttons.js
@@ -24,10 +24,16 @@ $(document).ready(function() {
 				extend:    'pdfHtml5',
 				text:      '<i class="fas fa-file-pdf"></i> ',
 				titleAttr: 'Exportar a PDF',
-				className: 'btn btn-danger'
-			}
-		]	        
+				className: 'btn btn-danger',
+				pageSize:{
+					width: 1000,
+					height: 1000
+				}
+				}
+			,
+		],
     });     
-});
+})
+
 
 
